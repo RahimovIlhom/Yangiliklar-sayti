fca5e030f595978a34ca2ecd6b035cddc935f20a
Ilhomjon Rahimov <94887912+RahimovIlhom@users.noreply.github.com>
2022-09-12 17:55:52 +0500

    Heroku uchun o'zgartirildi

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,3 @@
-from msilib.schema import CreateFolder
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,3 @@
-from operator import truediv
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
